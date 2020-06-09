5f18fa524ec4e3ca82e6503a28c9f4173b7ae05b
emackinnon1 <emackinnon100390@gmail.com>
2020-06-09 11:41:44 -0600

    integration test for app allowing user to search for exercises is passing

--- src/components/App/AppTestData.js
+++ src/components/App/AppTestData.js
@@ -28,6 +28,26 @@ export const exerciseTestData = {
 	],
 };
 
+export const exerciseSearchTestData = [
+	{
+		id: 97,
+		license_author: "wger.de",
+		status: "2",
+		description:
+			"<p>This exercise involves peeing on a carpet, just like a dog would. Lift your leg for added effect.</p>",
+		name: "Downward Dog",
+		name_original: "Downward Dog",
+		creation_date: null,
+		uuid: "0ec76f5d-1311-4d6d-bf79-00fa17c3061a",
+		license: 1,
+		category: 11,
+		language: 2,
+		muscles: [4],
+		muscles_secondary: [2, 5],
+		equipment: [8, 3],
+	},
+];
+
 export const categoryTestData = [
 	{
 		id: 10,
