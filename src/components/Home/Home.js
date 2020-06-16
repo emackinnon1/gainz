22c98af6410f188e986cffaefb555b813c09afe5
emackinnon1 <emackinnon100390@gmail.com>
2020-06-16 15:53:44 -0600

    Add instructions to Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,6 +11,16 @@ const Home = () => {
 				Use this app to turn those PVC pipes you call "arms" into magnificent
 				meat rods of power that even Mike Tyson would be proud of.
 			</p>
+			<p>
+				To use this app, pick the goal of the exercise, the muscle group you
+				would like to workout and the equipment you have available and click the
+				search button. Pick three to four exercises to add them to your initial
+				plan. You can search as many times as you want to find different
+				muscular goal, exercise and equipment combinations. Once you have a plan
+				that you like, add them to your saved routines. In your saved routines
+				you can see more information about the individual exercises and how many
+				reps and sets you should do for an optimal workout.
+			</p>
 			<Link className="get-huge-btn" to="/buildworkout">
 				LET'S GET HUGE
 			</Link>
